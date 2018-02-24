64683d2b7d4db6548fdb4cbb503aec004e8f80f2
Alex <gaoalexander73@gmail.com>
2018-02-23 18:33:44 -0700

    aditional files added so it can be deployed

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+This script runs the FlaskRegistrationServer application using a development server.
+"""
+
+from os import environ
+from src import app
+
+if __name__ == '__main__':
+    app.run( )
